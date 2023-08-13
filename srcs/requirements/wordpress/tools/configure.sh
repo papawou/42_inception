f96c4adc5342df8d9f48a07a2e1d62f62cc45e3e
Kevin Mendes <kmendes@c1r15s5.42lausanne.ch>
2023-08-13 14:35:44 +0200

    boumboum

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -12,7 +12,7 @@ while [ $success -eq 0 ]; do
     fi
 done
 
-wp core install --url=localhost --title="WP-CLI" --admin_user=$WP_ADMINNAME --admin_password=$WP_ADMINPWD --admin_email=$WP_ADMINEMAIL --skip-email --allow-root
+wp core install --url=$WP_DOMAINNAME --title=$WP_TITLE --admin_user=$WP_ADMINNAME --admin_password=$WP_ADMINPWD --admin_email=$WP_ADMINEMAIL --skip-email --allow-root
 wp theme install inspiro --activate --allow-root
 
 cd /
