3b8a9941a6e624842f1042e1ff811b6fe1d3c315
Kevin Mendes <kmendes@c3r2s5.42lausanne.ch>
2023-08-15 00:04:41 +0200

    fuck wiating mariadb, use of depends_on

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 
-wp core download --allow-root
-wp config create --dbname=$WP_DBNAME --dbuser=$WP_DBUSER --dbpass=$WP_DBPASSWORD --dbhost=mysql --skip-check --allow-root
-
-success=0
-while [ $success -eq 0 ]; do
-    wp db check --allow-root
-    if [ $? -eq 0 ]
-    then
-        success=1
-    fi
-done
-
 wp core install --url=$WP_DOMAINNAME --title=$WP_TITLE --admin_user=$WP_ADMINNAME --admin_password=$WP_ADMINPWD --admin_email=$WP_ADMINEMAIL --skip-email --allow-root
 wp theme install inspiro --activate --allow-root
 
-cd /
 /usr/sbin/php-fpm7.3 -F
