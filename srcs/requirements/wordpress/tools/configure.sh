80841b44f80d0c042bfe2eff1e3090ebe19cc00d
Kevin Mendes <kmendes@c1r2s4.42lausanne.ch>
2023-08-18 22:14:37 +0200

    missing volumes local

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-wp core install --url=$WP_DOMAINNAME --title=$WP_TITLE --admin_user=$WP_ADMINNAME --admin_password=$WP_ADMINPWD --admin_email=$WP_ADMINEMAIL --skip-email --allow-root
+wp core install --url=$WP_DOMAINNAME --title=$WP_TITLE --admin_user=$WP_ADMINNAME --admin_password=$WP_ADMINPWD --admin_email=$WP_ADMINMAIL --skip-email --allow-root
 wp theme install inspiro --activate --allow-root
+wp user create $WP_USERNAME $WP_USERMAIL --user_pass=$WP_USERPWD --allow-root
 
 /usr/sbin/php-fpm7.3 -F
